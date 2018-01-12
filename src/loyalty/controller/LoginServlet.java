6eab0910233c85261a53d49385992b90f9039cc3
wafakadori <31844743+wafakadori@users.noreply.github.com>
2018-01-11 21:24:53 -0700

    Update LoginServlet.java
    add first commit from github

--- src/loyalty/controller/LoginServlet.java
+++ src/loyalty/controller/LoginServlet.java
@@ -25,6 +25,7 @@ public class LoginServlet extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
+				System.out.println("addfirstcomit from github");
 		// get card number input and code input
 		String cardNumber = request.getParameter("cardNumber");
 		String cardCode = request.getParameter("cardCode");
