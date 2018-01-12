285d1dbaaaad13a4b08cac637a4ca73d19869cbc
wafakadori <31844743+wafakadori@users.noreply.github.com>
2018-01-11 23:03:11 -0700

    Update LoginServlet.java
    test

--- src/loyalty/controller/LoginServlet.java
+++ src/loyalty/controller/LoginServlet.java
@@ -20,7 +20,7 @@ public class LoginServlet extends HttpServlet {
 
 		System.out.println("add a new commit 5555555");
 
-		System.out.println("add a new commit 66666");
+		System.out.println("add a new commit 8888");
 
 
 		// get card number input and code input
